47bddf17b031cba290ef63689a954e58dd505a58
Lois <loisisar@outlook.com>
2023-11-22 19:54:33 +0000

    default install live grep?

--- lua/lmp/plugins/telescope.lua
+++ lua/lmp/plugins/telescope.lua
@@ -1,4 +1,6 @@
 return {
+  {
+    
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = {
@@ -8,4 +10,11 @@ return {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
   },
-}
+
+
+  }, {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    branch="master",
+    version="^1.0.0",
+  }
+  }
